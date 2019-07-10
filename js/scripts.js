4f6e8c9850abd03b40b9150a01608f85b196a20c
kimaurellano <altaiirdesmond@gmail.com>
2019-07-10 15:10:15 +0800

    Added map emphasis on hover and real-time clock

--- js/scripts.js
+++ js/scripts.js
@@ -26,10 +26,6 @@ function openPage(pageToOpen, element) {
   element.style.color = "#1979CA";
 }
 
-function defaultTab() {
-  document.getElementById("defaultOpen").click();
-}
-
 function loadLineChart() {
   var query = "SELECT * FROM Soil_Data";
   $.ajax({
@@ -38,18 +34,22 @@ function loadLineChart() {
     success: function (result) {
       var jsonData = JSON.parse(result);
 
+      // The x-axis label of chart
       var labels = jsonData.map((e) => {
         return e.time;
       });
 
+      // Dataset of nitrogen
       var n = jsonData.map((e) => { 
         return e.nitrogen;
       });
 
+      // Dataset of phosphorous
       var p = jsonData.map((e) => { 
         return e.phosphorous;
       });
 
+      // Dataset of potassium
       var k = jsonData.map((e) => { 
         return e.potassium;
       });
@@ -84,8 +84,89 @@ function loadLineChart() {
   });
 }
 
+function getNPKContent(soilType) {
+  $(document).ready(function () {
+    // Fetch the soil type
+    soilType = soilType.split(" ")[0].toLowerCase();
+
+    // Sets new colour
+    var color;
+    if(soilType == "nitrogen"){
+      color = "#0000FF";
+    } else if(soilType == "phosphorous"){
+      color = "#A6FF00";
+    } else if(soilType == "potassium"){
+      color = "#FF5B00";
+    }
+    
+    // Apply
+    $(".f-path").css({
+        "fill": color
+    });
+
+    var query = "SELECT " + soilType + " FROM Soil_Data ORDER BY id DESC LIMIT 6";
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "php/NPKContents.php?query=" + query, true);
+    xmlhttp.send();
+      
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
+        // The result we get from the URL
+        var jsonData = JSON.parse(xmlhttp.responseText);
+        var idx = 1;
+        jsonData.forEach(element => {
+          document.getElementById("field-" + idx).innerHTML = "Field " + idx + ": " + element[soilType] + "%";
+          $(".f" + idx).css({
+            "opacity": element[soilType] / 100
+          });
+          idx += 1;
+        });
+      }
+    }
+  });
+}
+
+function refreshTime() {
+  var dateString = new Date().toLocaleString("en-US", {timeZone: "Singapore"});
+  var formattedString = dateString.replace(", ", " - ");
+  console.log(formattedString);
+  document.getElementById("real-time").innerHTML = formattedString;
+}
+
+setInterval(refreshTime, 1000);
+
 /* EVENT LISTENERS */
 window.addEventListener("load", () => { 
   $("#myTable").DataTable();
+
+  document.getElementById("defaultOpen").click();
+  
+  getNPKContent("phosphorous");
+
+  // Map field emphasis on hover
+  for (let index = 1; index < 7; index++) {
+    document.getElementById("field-" + index).addEventListener("mouseover", function () { 
+      $("#field-" + index).css({
+        "font-weight": "bold"
+      });
+
+      $(".f" + index).css({
+        "stroke": "#fff",
+        "stroke-width": "5px"
+      });
+    })
+
+    document.getElementById("field-" + index).addEventListener("mouseleave", function () { 
+      $("#field-" + index).css({
+        "font-weight": "300"
+      });
+
+      $(".f" + index).css({
+        "stroke": "none",
+        "stroke-width": "1px"
+      });
+    })
+  }
 });
 
