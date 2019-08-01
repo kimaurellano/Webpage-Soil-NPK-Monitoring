e5731129fabcf36f725ea3c36c0384a16af37da7
Kim Aurellano <34935665+kimaurellano@users.noreply.github.com>
2019-08-01 11:32:50 +0800

    Update DatabaseManager.php

--- php/DatabaseManager.php
+++ php/DatabaseManager.php
@@ -1,5 +1,5 @@
 <?php 
-	$connection = mysqli_connect("2.57.89.1", "u690747639_root", "0OzJ2dik0HEZ", "u690747639_soil");
+	$connection = mysqli_connect("localhost", "u690747639_root", "0OzJ2dik0HEZ", "u690747639_soil");
         
 	// Check connection
 	if (!$connection) {
