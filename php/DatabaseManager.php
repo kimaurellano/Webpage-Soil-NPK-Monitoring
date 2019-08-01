7e88ac15d269f6e0d468fe134c3c0d9295a79373
kimaurellano <kim.aurellano@yahoo.com>
2019-08-01 11:58:36 +0800

    Reverted test change

--- php/DatabaseManager.php
+++ php/DatabaseManager.php
@@ -1,6 +1,5 @@
 <?php 
 	$connection = mysqli_connect("localhost", "u690747639_root", "0OzJ2dik0HEZ", "u690747639_soil");
-    //test change
 	// Check connection
 	if (!$connection) {
 		echo $connection -> error;
