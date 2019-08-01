fbf72012dc6e90f4d098bb9fe16ce191049f4fae
kimaurellano <kim.aurellano@yahoo.com>
2019-08-01 11:48:31 +0800

    Test change

--- php/DatabaseManager.php
+++ php/DatabaseManager.php
@@ -1,6 +1,6 @@
 <?php 
-	$connection = mysqli_connect("2.57.89.1", "u690747639_root", "0OzJ2dik0HEZ", "u690747639_soil");
-        
+	$connection = mysqli_connect("localhost", "u690747639_root", "0OzJ2dik0HEZ", "u690747639_soil");
+    //test change
 	// Check connection
 	if (!$connection) {
 		echo $connection -> error;
